802c41265fa90f8835d83bafa648f07a55d8370b
Cat Georgoulis <catmou@gregorys-MacBook-Pro.local>
2016-05-10 14:31:37 -0400

    start setBet submitBet

--- bjapp.js
+++ bjapp.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 	var player = {
 		name: "player",
 		money: 200,
+		currentBet: 0 ,
 		loss: loseMoney = function(loss){
 			player.money = player.money - loss;
 		},
@@ -12,24 +13,76 @@ $(document).ready(function(){
 
 		},
 		currentHand: hand = [2,4,6],
-		gethandVal: handVal = function(){
-								sum = 0
-								for (var i = 0; i < hand.length; i++) {
-									sum += hand[i];
-								}
-								return sum;	
-							},
+		gethandVal: 
+		handVal = function(){
+			sum = 0
+			for (var i = 0; i < hand.length; i++) {
+				sum += hand[i];
+				}
+			return sum;	
+			},
 		handValue: 	handVal(),
 		
 		handsWon: 0,
 		handsLoss: 0
 
 	}
+	var dealer = {
+		name: "dealer",
+		currentHand: hand = [2,4,6],
+		gethandVal: 
+		handVal = function(){
+			sum = 0
+			for (var i = 0; i < hand.length; i++) {
+				sum += hand[i];
+				}
+			return sum;	
+		},
+		handValue: 	handVal(),
+		
+		handsWon: 0,
+		handsLoss: 0
+
+	};
 
 	var dummyDeck = [1,2,3,4,5,6,7,8,9,10,11];
 
+//////////////////////////////////////
+// the star function begins the game and asks player to place a bet
 
-	console.log(player.handValue);
+	$("#start").click(function(){
+		console.log("working");
+		$("#dealer").html("dealer");
+		$("#player").html("player");
+		setBet()
+	})
+
+	var setBet = function(){
+		var $select = $("<select></select>");
+		$select.attr("id","bet");
+		$select.append($("<option>5</option>"));
+		$select.append($("<option>15</option>"));
+		$select.append($("<option>25</option>"));
+		$("#player").append($select);
+		$("#start").remove();
+		var $submit = $("<button>sumbit</button>");
+		$submit.attr("id","submit");
+		$("#player").append($submit);
+		submitBet();
+	}
+
+	var submitBet = function(){
+		$("#submit").click(function(){
+			player.currentBet = $("#bet").val();
+			console.log(player.currentBet);
+			
+		})
+	}
+
+
+
+
+
 
 
 	
