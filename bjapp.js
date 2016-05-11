91d1f741e21b837179d7041effa720642b45ec6b
Cat Georgoulis <catmou@gregorys-MacBook-Pro.local>
2016-05-11 11:59:15 -0400

    check for winner loser next hand

--- bjapp.js
+++ bjapp.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 // dummy array for testing
 
 
-	var dummyDeck = [2,3,4,5,6,7,8,9,10,11,2,3,4,5,6,7,8,9,10,11];
+	var dummyDeck = [2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11];
 
 
 	var player = {
@@ -14,30 +14,26 @@ $(document).ready(function(){
 		money: 200,
 		currentBet: 0 ,
 		loss: loseMoney = function(loss){
-			player.money = player.money - loss;
+			var theMoney = parseInt(player.money);
+			player.money = theMoney - loss;
 		},
 		gain: gainMoney = function(gain){
-			player.money = player.money + gain;
+			var theMoney = parseInt(player.money);
+			player.money = theMoney + gain;
 		},
 		currentHand: 	
 			getHand = function(){
-				var num = Math.floor(Math.random()*19 );
+				var num = Math.floor(Math.random()*25 );
 				var hand = dummyDeck.splice(num,2);
-				console.log(hand);
+				var hand1 = parseInt(hand[0]);
+				var hand2 = parseInt(hand[1]);
+				player.handValue += hand1 + hand2;
+				console.log(player.handValue);
+				// console.log(hand);
 				$("#pcards").html(hand[0] + " ," + hand[1]);
 			},
-		// gethandVal: 
-		// 	handVal = function(){
-		// 		getHand();
-		// 		sum = 0
-		// 		for (var i = 0; i < hand.length; i++) {
-		// 			sum += hand[i];
-		// 			}
-		// 		return sum;	
-		// 	},
-		// handValue: 	handVal(),	
-		// handsWon: 0,
-		// handsLoss: 0
+			 handValue: 0
+	
 	}
 
 	//===========================================================
@@ -45,12 +41,15 @@ $(document).ready(function(){
 		name: "dealer",
 		currentHand: 	
 			getHand1 = function(){
-				var num = Math.floor(Math.random()*17 );
+				var num = Math.floor(Math.random()*22 );
 				var hand = dummyDeck.splice(num,2);
-				console.log(hand);
+				var hand1 = parseInt(hand[0]);
+				var hand2 = parseInt(hand[1]);
+				dealer.handValue += hand1 + hand2;
+				// console.log(hand);
 				$("#dcards").html(hand[0] + " ," + hand[1]);
 			},
-		
+		handValue: 0,		
 		handsWon: 0,
 		handsLoss: 0
 	};
@@ -60,14 +59,17 @@ $(document).ready(function(){
 // the star function begins and invokes the setBet function
 
 	$("#start").click(function(){
-		console.log("working");
+		// console.log("working");
 		$("#dealer").html("dealer");
 		$("#player").html("player");
-		setBet()
+		$("#money").html(player.money);
+		setBet();
 	})
+
 /////the set bet function adds a drop list and a sumit button, removes the start button
 //and invokes submitBet function
 	var setBet = function(){
+		dummyDeck = [2,3,4,5,6,7,8,9,10,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,10,11];
 		var $select = $("<select></select>");
 		$select.attr("id","bet");
 		$select.append($("<option>5</option>"));
@@ -83,24 +85,156 @@ $(document).ready(function(){
 /// this function occurs when the submit button is clicked and it takes the selected value and sets the player's
 // currentBet value and invokes the deal function??
 	var submitBet = function(){
-		$("#submit").click(function(){
+		if (player.money > 0) {
+			$("#submit").click(function(){
 			player.currentBet = $("#bet").val();
+			$("h2").html(player.currentBet);
 			
 			deal();
-		})
+			})
+		}
 	}
 // takes functions from player and dealer to get their frst two cards
-var deal = function(){
-	$("#bet").remove();
-	$("#submit").remove();
-	 getHand();
-	 getHand1();
-}
+	var deal = function(){
+		$("#bet").remove();
+		$("#submit").remove();
+		// console.log(dummyDeck);
+		 getHand();
+		 getHand1();
+		 addHit();
+		 addStand();
+	}
 
+	var addHit = function(){
+		// console.log($("#pcards").html());
+		var hitB = $("<button>Hit Me</button>").attr("id","hitMe");
+		$("#hit").append(hitB);
+		hitPlayer();
 
+	}
+	var addStand = function(){
+		var standB = $("<button>stand</button>").attr("id","standMe");
+		$("#hit").append(standB);
+		standPlayer();
 
+	}
+	var hitPlayer = function(){
+		 var hitCounter = 0;	
+		 console.log(player.currentBet);
+		$("#hitMe").click(function(){
+			hitCounter ++;
+			var num = Math.floor(Math.random()*20 );
+			var hand = dummyDeck.splice(num,1);
+			var one = $("#pcards").html();
+			var hand1 = parseInt(hand);
+			player.handValue += hand1;
+			console.log(player.handValue);
+			$("#pcards").html(one + " ," + hand);
+			if (player.handValue > 21) {
+				$("#hitMe").unbind("click");
+				alert(" so sorry, you bust");
+				var loss = parseInt(player.currentBet);
+				loseMoney(loss);
+				alert("- $" + player.currentBet);
+				$("#money").html(player.money);
+				nextHand();
+				dealerTurn();
 
+			}
+			if (hitCounter === 3) {
+				$("#hitMe").unbind("click");
+			}
+		})
+	}
+
+	var standPlayer = function(){
+		$("#standMe").click(function(){
+			$("#hitMe").remove();
+			dealerTurn();
+		})
+
+		
+	}
+
+	var dealerTurn = function(){
+		while (dealer.handValue < 17){
+		var num = Math.floor(Math.random()*15 );
+		var hand = dummyDeck.splice(num,1);
+		var one = $("#dcards").html();
+		var hand1 = parseInt(hand);
+		dealer.handValue += hand1;
+		$("#dcards").html(one + " ," + hand);
+	}
+		if (dealer.handValue > 21) {	
+				alert("dealer bust yesssssssaahhhh");	
+				var gain = parseInt(player.currentBet);
+				gainMoney(gain);
+				alert("+ $" + player.currentBet);
+				$("#money").html(player.money);	
+				// nextHand();	
+			}
+		console.log(dealer.handValue);
+		checkWinner();
+		checkLoser();
+	}
+
+	var checkWinner = function(){
+		if( player.handValue < 22 && dealer.handValue < 22){
+			if (player.handValue > dealer.handValue) {
+			var gain = parseInt(player.currentBet);
+			gainMoney(gain);
+			alert("+ $" + player.currentBet);
+			$("#money").html(player.money);
+			console.log(player.money);
+			nextHand();
+			}else if (dealer.handValue > 21) {
+			gainMoney(gain);
+			alert("+ $" + player.currentBet);
+			$("#money").html(player.money);
+			nextHand();
+			}else if (player.handValue === dealer.handValue) {
+				alert("push");
+				nextHand();
+			}
+		}
+		
+	}
+
+	var checkLoser = function(){
+		if( player.handValue < 22 && dealer.handValue < 22){
+			if (player.handValue < dealer.handValue) {
+			var loss = parseInt(player.currentBet);
+			loseMoney(loss);
+			alert("- $" + player.currentBet);
+			$("#money").html(player.money);
+			console.log(player.money);
+			nextHand();
+			}
+		}	
+	}	
+
+	var nextHand = function(){
+		console.log("working");
+		$("#hitMe").remove();
+		$("#standMe").remove();
+		var nextB = $("<button>next hand</button>").attr("id","next");
+		$("#hit").append(nextB);
+		newHand();
+	}
 	
+	var newHand = function(){
+		$("#next").click(function(){
+			$("h2").html("");
+			$("#pcards").html("");
+			$("#dcards").html("");
+			player.handValue = 0;
+			dealer.handValue = 0;
+			setBet();
+			$("#next").remove();
+
+		})
+
+	}
 
 
 
@@ -129,6 +263,18 @@ var deal = function(){
 	// 	// console.log(dealer.currentHand);
 
 	// }
+		// gethandVal: 
+		// 	handVal = function(){
+		// 		getHand();
+		// 		sum = 0
+		// 		for (var i = 0; i < hand.length; i++) {
+		// 			sum += hand[i];
+		// 			}
+		// 		return sum;	
+		// 	},
+		// handValue: 	handVal(),	
+		// handsWon: 0,
+		// handsLoss: 0
 		// currentHand: ,
 		// gethandVal: 
 		// handVal = function(){
@@ -139,3 +285,7 @@ var deal = function(){
 		// 	return sum;	
 		// },
 		// handValue: 	handVal(),	
+				// var $makeDiv = $("<div></div>");
+		// $($makeDiv).html(hand[0]);
+		// $("#pcards").append($makeDiv);
+		// console.log($("#pcards").html());	
